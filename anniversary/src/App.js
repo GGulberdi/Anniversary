8c542031c6474c47d88e18616bc3dbbfa9afeecf
Gulsenem Gulberdi <ggulberdiyeva@gmail.com>
2021-10-06 17:00:06 -0500

    first commit

---
+++ anniversary/src/App.js
@@ -0,0 +1,19 @@
+import desert from './images/desert_photo.jpg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='cover'><p>Our Journey started at January 6 2007y</p></div>
+
+
+        
+        <img src={desert} className="App-logo" alt="logo" />
+         </header>
+    </div>
+  );
+}
+
+export default App;
