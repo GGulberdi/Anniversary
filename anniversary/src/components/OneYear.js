98f53d4a37c88562f34f211e05ba450573652c27
Gulsenem Gulberdi <ggulberdiyeva@gmail.com>
2021-10-20 05:21:57 -0500

    Added css to oneyear.js

--- anniversary/src/components/OneYear.js
+++ anniversary/src/components/OneYear.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import surat from '../images/soul.jpg'
+import '../css/oneyear.css'
 
 export default function OneYear() {
     return (
         <div>
-            Welcome to OneYear year
+        <h1>Our First year </h1>
+        <p>It was beatiful year and we were prepearing for new family member to join our family </p>
+
+<img src={surat} alt='firstyear'/>
         </div>
     )
 }
