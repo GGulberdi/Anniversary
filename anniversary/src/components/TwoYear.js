54b13d4fb641f4625096d08f8d1d4fb8d43f8333
Gulsenem Gulberdi <ggulberdiyeva@gmail.com>
2021-11-05 23:34:01 -0500

    added yeartwo  css

--- anniversary/src/components/TwoYear.js
+++ anniversary/src/components/TwoYear.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import '../css/twoyear.css'
+import mainphoto from '../images/yeartwo.PNG'
 
 export default function TwoYear() {
     return (
-        <div>
-            Welcome to TwoYear year
+        <div className='container'>
+            <p>Bilelikdäki durmuşymyzyň ikinji ýylynda biziň öňümize uzyn ýollar çykdy, we bir Amerikanyň birleşen şatlaryna göçüp gaýtdyk. Bu bir uly ädimdi we henize çhenli yashayan ulkämiz bolan Amerika bizi gaty gowy garşy aldy.</p>
+            <img src={mainphoto} alt='main_photo'></img>
         </div>
     )
 }
